b3d13bfd79b9c57fc044baff7c3f9f80239d9bd9
Koqeez <61950289+Koqeez@users.noreply.github.com>
2022-05-02 00:27:44 +0200

    0.2

--- K-means-clustering/Source.cc
+++ K-means-clustering/Source.cc
@@ -8,25 +8,27 @@
 using namespace std;
 
 struct Point {
-	double x, y;
+	double x, y, z;
 	int cluster;
 	double minDist;
 
 	Point() :
 		x(0.0),
 		y(0.0),
+		z(0.0),
 		cluster(-1),
 		minDist(DBL_MAX) {
 
 	}
-	Point(double x, double y):
+	Point(double x, double y,double z):
 		x(x),
 		y(y),
+		z(z),
 		cluster(-1),
 		minDist(DBL_MAX){ 
 	}
 	double distance(Point p) {
-		return (p.x - x) * (p.x - x) + (p.y - y) * (p.y - y);
+		return (p.x - x) * (p.x - x) + (p.y - y) * (p.y - y) + (p.z - z) * (p.z-z);
 	}
 };
 
@@ -38,12 +40,14 @@ vector<Point> readcsv() {
 	while (getline(file, line)) {
 		stringstream lineStream(line);
 		string bit;
-		double x, y;
+		double x, y, z;
 		getline(lineStream, bit, ',');
 		x = stof(bit);
-		getline(lineStream, bit, '\n');
+		getline(lineStream, bit, ';');
 		y = stof(bit);
-		points.push_back(Point(x, y));
+		getline(lineStream, bit, '\n');
+		z = stof(bit);
+		points.push_back(Point(x, y, z));
 	}
 	return points;
 }
@@ -73,12 +77,13 @@ void kMeansClustering(vector<Point>* points, int epochs, int k) {
 		}
 	}
 	vector<int> nPoints;
-	vector<double> sumX, sumY;
+	vector<double> sumX, sumY,sumZ;
 
 	for (int j = 0; j < k; ++j) {
 		nPoints.push_back(0);
 		sumX.push_back(0.0);
 		sumY.push_back(0.0);
+		sumZ.push_back(0.0);
 	}
 
 	for (vector<Point>::iterator it = points->begin();
@@ -87,6 +92,7 @@ void kMeansClustering(vector<Point>* points, int epochs, int k) {
 		nPoints[clusterId] += 1;
 		sumX[clusterId] += it->x;
 		sumY[clusterId] += it->y;
+		sumZ[clusterId] += it->z;
 
 		it->minDist = DBL_MAX;  
 	}
@@ -96,14 +102,15 @@ void kMeansClustering(vector<Point>* points, int epochs, int k) {
 		int clusterId = c - begin(centroids);
 		c->x = sumX[clusterId] / nPoints[clusterId];
 		c->y = sumY[clusterId] / nPoints[clusterId];
+		c->z = sumZ[clusterId] / nPoints[clusterId];
 	}
 	ofstream myfile;
 	myfile.open("output.csv");
-	myfile << "x,y,c" << endl;
+	myfile << "x,y,z,c" << endl;
 
 	for (vector<Point>::iterator it = points->begin(); it != points->end();
 		++it) {
-		myfile << it->x << "," << it->y << "," << it->cluster << endl;
+		myfile << it->x << "," << it->y << "," <<it->z<<"," << it->cluster << endl;
 	}
 	myfile.close();
 }
